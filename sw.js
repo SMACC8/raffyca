27e8003239e6679d538e784b14e27a90df020477
SMACC8 <94976500+SMACC8@users.noreply.github.com>
2026-06-21 21:46:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).catch(()=>{})));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE && k.indexOf('raffyca-rt-') !== 0).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
   const req = e.request;
